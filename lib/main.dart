08d84945a29ebf9c7ded9811cf592718ab5c3c25
aebash <localization.helloWorld>
2022-12-03 12:29:32 +0900

    Firebase Authの追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo/src/views/home_screen.dart';
+
+import 'src/logger.dart';
 
 void main() {
   runApp(const App());
@@ -12,6 +15,26 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        logger.d('User is currently signed out!');
+      } else {
+        logger.d('User is signed in!');
+      }
+    });
+
+    try {
+      final userCredential = FirebaseAuth.instance.signInAnonymously();
+      logger.d("Signed in with temporary account.");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          logger.d("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          logger.d("Unknown error.");
+      }
+    }
     return const MaterialApp(
       title: 'Template App',
       localizationsDelegates: [
