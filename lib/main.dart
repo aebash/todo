9e638b406f0273689b67a6250e45be052969f5b7
aebash <localization.helloWorld>
2022-12-03 14:01:52 +0900

    initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo/src/views/home_screen.dart';
 
+import 'firebase_options.dart';
 import 'src/logger.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
 
